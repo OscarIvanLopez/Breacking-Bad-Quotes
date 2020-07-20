ae317311760e45371e305101ee44709f56578866
OscarIvanLopez <oscari.lopez@potros.itson.edu.mx>
2020-07-19 19:29:04 -0700

    Updates

---
+++ precache-manifest.cdf834eddae75ee3b068e0318b7d28cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03927b52e1672a3b81e984606f937df",
+    "url": "/Breacking-Bad-Quotes/index.html"
+  },
+  {
+    "revision": "28da37379b81eeb304ef",
+    "url": "/Breacking-Bad-Quotes/static/css/main.7fd52425.chunk.css"
+  },
+  {
+    "revision": "aec822964fa8caba39b2",
+    "url": "/Breacking-Bad-Quotes/static/js/2.bdc7eff5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Breacking-Bad-Quotes/static/js/2.bdc7eff5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28da37379b81eeb304ef",
+    "url": "/Breacking-Bad-Quotes/static/js/main.f9e921e8.chunk.js"
+  },
+  {
+    "revision": "76d97432d0b3d595ae3d",
+    "url": "/Breacking-Bad-Quotes/static/js/runtime-main.5b2a39c9.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/Breacking-Bad-Quotes/static/media/logo.427a4adc.svg"
+  }
+]);
